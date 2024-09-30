ca5b732af4eb6005110a3ecb923b772ca75850e0
ryeong99 <lovolvo1015@gmail.com>
2024-09-30 16:14:14 +0900

    app.jar modify

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ ARG JAR_FILE=target/*.jar
 ARG PROFILES
 COPY ${JAR_FILE} app.jar
 #ARG는 서버 실행 시 변수로 넣어줄 변수명을 지정
-ENTRYPOINT ["java", "-Dspring.profiles.active=${PROFILES}", "-jar", ".app.jar"]
+ENTRYPOINT ["java", "-Dspring.profiles.active=${PROFILES}", "-jar", "app.jar"]
--- src/main/java/com/deploy/deploy_back/controller/HealthCheckController.java
+++ src/main/java/com/deploy/deploy_back/controller/HealthCheckController.java
@@ -14,13 +14,15 @@ public class HealthCheckController {
 
     @Value("${spring.profiles.active}")
     private String env;
+    // 실행중인 서버
 
     @Value("${server.port}")
     private String port;
+    // 실행중인 포트
 
     @GetMapping("/health/check")
     public ResponseEntity<?> healthCheck() {
-        Map<String, Objects> response = new HashMap<>();
+        Map<String, Object> response = new HashMap<>();
         response.put("env", env);
         response.put("port", port);
         return ResponseEntity.ok().body(response);
